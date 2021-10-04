9a5bb02cb4bb7412e83fe01ea9dd00dd114281bd
Curtis Slone <curtisdslone@gmail.com>
2021-10-04 12:12:54 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Video from './components/Video'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__videos">
+      <Video 
+        url="https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
+        channel="the_user1"
+        description="A Short Video"
+        likes={345}
+        shares={200}
+        messages={90}
+      />
+      <Video 
+        url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
+        channel="theUSER333"
+        description="Another Short Video"
+        likes={145}
+        shares={20}
+        messages={67}
+      />
+      </div>
     </div>
   );
 }
